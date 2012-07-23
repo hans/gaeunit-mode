f695a3b32d166c262bac77b5bbfc4e767ee08592
Hans Engel <engel@engel.uk.to>
2012-07-22 20:29:44 -0700

    Determine success / failure of test run and call hooks accordingly

--- gaeunit-mode.el
+++ gaeunit-mode.el
@@ -1,6 +1,20 @@
+(defvar gaeunit-tests-success-hook nil
+  "Hook called when all GAEUnit tests succeed")
+
+(defvar gaeunit-tests-failure-hook nil
+  "Hook called when one or more GAEUnit tests fail")
+
 (defun gaeunit-run-tests ()
   (interactive)
-  (call-process "gaeunit" nil "*gaeunit*" nil))
+  (call-process "gaeunit" nil "*gaeunit*" nil)
+  (save-current-buffer
+    (switch-to-buffer "*gaeunit*" t)
+
+    (let ((buffer-contents (buffer-string)))
+      (run-hooks
+       (if (string-match-p "^OK$" buffer-contents)
+          'gaeunit-tests-success-hook
+          'gaeunit-tests-failure-hook)))))
 
 (defvar gaeunit-mode-map (make-sparse-keymap)
   "gaeunit-mode keymap")
